0a550ab0e9d27832591736e277ee3bd008b990b0
nirupamkumar <mundra.nirupamkumar@gmail.com>
2024-11-03 13:32:15 +0100

    Player Scripts

---
+++ Assets/Scripts/Player/PlayerDie.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDie : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.gameObject.tag == "Deadly")
+        {
+            OnExplode();
+            SceneManager.LoadScene("Level2", LoadSceneMode.Single);
+        }
+    }
+
+    void OnExplode()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerDieTwo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDieTwo : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.gameObject.tag == "Deadly")
+        {
+            OnExplode();
+            SceneManager.LoadScene("Level3.1", LoadSceneMode.Single);
+        }
+    }
+
+    void OnExplode()
+    {
+        Destroy(gameObject);
+    }
+}
