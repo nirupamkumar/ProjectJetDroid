bb1e5d57472a90dfb2ad9985b2e433461a954103
nirupamkumar <mundra.nirupamkumar@gmail.com>
2024-11-03 13:33:27 +0100

    UI Scripts

---
+++ Assets/Scripts/UI/CreditsExit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsExit : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Pressed primary button.");
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            ExitButton();
+    }
+
+    void ExitButton()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+}
