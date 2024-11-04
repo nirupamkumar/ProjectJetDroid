453c01bf4247f645af62023270e841340bc9ef89
nirupamkumar <mundra.nirupamkumar@gmail.com>
2024-11-04 19:02:21 +0100

    Update Collectables.cs

--- Assets/Scripts/Interactions/Collectables.cs
+++ Assets/Scripts/Interactions/Collectables.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Collectables : MonoBehaviour
 {
+    public AudioClip pickupsound;
+
     private void OnTriggerEnter2D(Collider2D target)
     {
         if(target.gameObject.tag == "Player")
         {
+            if (pickupsound)
+            {
+                AudioSource.PlayClipAtPoint(pickupsound, transform.position);
+            }
+
             Destroy(gameObject);
         }
     }
