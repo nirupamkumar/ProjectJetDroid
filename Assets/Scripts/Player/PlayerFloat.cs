caab848afed02985bba322f2d9b0efa23e02cbad
nirupamkumar <mundra.nirupamkumar@gmail.com>
2024-11-10 03:15:26 +0100

    Update PlayerFloat.cs

--- Assets/Scripts/Player/PlayerFloat.cs
+++ Assets/Scripts/Player/PlayerFloat.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class PlayerFloat : MonoBehaviour
 {
-    
+    private float amplitude = 0.15f;
+    private float frequency = 1.5f;
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        float newY = startPosition.y + Mathf.Sin(Time.time * frequency) * amplitude;
+        transform.position = new Vector3(startPosition.x, newY, startPosition.z);
+    }
 }
